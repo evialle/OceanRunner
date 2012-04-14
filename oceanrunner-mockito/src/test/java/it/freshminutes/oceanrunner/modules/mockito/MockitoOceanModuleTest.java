380b76c07f7256deefd527ae427ddb4c7f273bb2
Eric VIALLE <eric@vialle.org>
2012-04-14 03:59:38 +0200

    Improving Mockito

--- oceanrunner-mockito/src/test/java/it/freshminutes/oceanrunner/modules/mockito/MockitoOceanModuleTest.java
+++ oceanrunner-mockito/src/test/java/it/freshminutes/oceanrunner/modules/mockito/MockitoOceanModuleTest.java
@@ -4,16 +4,25 @@
 package it.freshminutes.oceanrunner.modules.mockito;
 
 import static org.junit.Assert.*;
+import it.freshminutes.oceanrunner.OceanRunner;
+import it.freshminutes.oceanrunner.annotations.OceanModulesToUse;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 
 /**
  * @author Eric
  *
  */
+@RunWith(OceanRunner.class)
+@OceanModulesToUse(MockitoOceanModule.class)
 public class MockitoOceanModuleTest {
 
+	@Mock
+	OceanRunner oceanRunnerMock;
+	
 	/**
 	 * @throws java.lang.Exception
 	 */
@@ -23,7 +32,7 @@ public class MockitoOceanModuleTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertNotNull(oceanRunnerMock);
 	}
 
 }
